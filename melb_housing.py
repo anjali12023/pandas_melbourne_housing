cb65807c49b57e43e7d6ae18383bbe5a16e297f7
anjali12023 <4njalisharma007@gmail.com>
2026-07-19 22:50:06 +1000

    price by suburb

--- melb_housing.py
+++ melb_housing.py
@@ -3,13 +3,11 @@ import pandas as pd
 df = pd.read_csv("data.csv")
 # print(df.loc[0])
 # Average house price
-group = df.groupby("suburb")
-av_price = group["price"].mean()
-print(av_price)
-
+average_price = df["price"].mean()
+print(average_price)
 
 # Price by suburb
-# Bedrooms vs price
-# Best value suburbs
-
+group = df.groupby("suburb")
+av_price = group["price"].mean()
+# print(av_price)
 
