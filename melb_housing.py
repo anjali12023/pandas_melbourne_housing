ab311a747158898e083403900336c6464f0dd4ab
anjali12023 <4njalisharma007@gmail.com>
2026-07-19 22:55:05 +1000

    average price per bedroom

--- melb_housing.py
+++ melb_housing.py
@@ -4,10 +4,17 @@ df = pd.read_csv("data.csv")
 # print(df.loc[0])
 # Average house price
 average_price = df["price"].mean()
-print(average_price)
+# print(average_price)
 
 # Price by suburb
 group = df.groupby("suburb")
 av_price = group["price"].mean()
 # print(av_price)
 
+
+#average price per bedroom
+group = df.groupby("bedrooms")
+av_price = group["price"].mean()
+print(av_price)
+
+# Bedrooms vs price
